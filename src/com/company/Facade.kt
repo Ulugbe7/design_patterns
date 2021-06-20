9b44f76ae7056ebfe53b8376bdbd38b916bf18ca
YusupovUlugbek <ulugbekyusupov272@gmail.com>
2021-06-20 17:47:35 +0500

    Facade commit

--- src/com/company/Facade.kt
+++ src/com/company/Facade.kt
@@ -1,4 +1,52 @@
 package com.company
 
-class Facade {
+fun main() {
+
+    val shapeMaker = ShapeMaker()
+
+    shapeMaker.drawCircleF()
+    shapeMaker.drawRecF()
+    shapeMaker.drawSquareF()
+
+}
+
+
+class ShapeMaker {
+    private val recF: ShapeFacade = RecF()
+    private val circleF: ShapeFacade = CircleF()
+    private val squareF: ShapeFacade = SquareF()
+
+    fun drawRecF() {
+        recF.draw()
+    }
+
+    fun drawCircleF() {
+        circleF.draw()
+    }
+
+    fun drawSquareF() {
+        squareF.draw()
+    }
+}
+
+interface ShapeFacade {
+    fun draw()
+}
+
+class RecF : ShapeFacade {
+    override fun draw() {
+        println("this is Rec class")
+    }
+}
+
+class CircleF : ShapeFacade {
+    override fun draw() {
+        println("this is Circle class")
+    }
+}
+
+class SquareF : ShapeFacade {
+    override fun draw() {
+        println("this is Square class")
+    }
 }
