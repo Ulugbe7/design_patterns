7a2391557e6d54c3081226bf153086b75eadd51b
YusupovUlugbek <ulugbekyusupov272@gmail.com>
2021-06-20 16:01:01 +0500

    Bridge commit

--- src/com/company/Bridge.kt
+++ src/com/company/Bridge.kt
@@ -1,4 +1,37 @@
 package com.company
 
-class Bridge {
+fun main() {
+
+    val redCircle: Shape = Circle(10, 3, 4, RedCircle())
+    val blueCircle: Shape = Circle(10, 3, 4, BlueCircle())
+
+    blueCircle.draw()
+    redCircle.draw()
+}
+
+interface Draw {
+    fun drawCircle(r: Int, x: Int, y: Int)
+}
+
+
+class RedCircle : Draw {
+    override fun drawCircle(r: Int, x: Int, y: Int) {
+        println("this is red circle r=$r, x=$x, y=$y")
+    }
+}
+
+class BlueCircle : Draw {
+    override fun drawCircle(r: Int, x: Int, y: Int) {
+        println("this is blue circle r=$r, x=$x, y=$y")
+    }
+}
+
+abstract class Shape(private val draw: Draw) {
+    abstract fun draw()
+}
+
+class Circle(private val r: Int, private val x: Int, private val y: Int, private val draw: Draw) : Shape(draw) {
+    override fun draw() {
+        draw.drawCircle(r, x, y)
+    }
 }
