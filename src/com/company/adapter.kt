4920215d2699f2306b280082e314da95cbc0f120
YusupovUlugbek <ulugbekyusupov272@gmail.com>
2021-06-20 15:27:19 +0500

    adapter commit

--- src/com/company/adapter.kt
+++ src/com/company/adapter.kt
@@ -1,5 +1,41 @@
 package com.company
 
 fun main() {
+    val con = true
+    var tasks: Tasks? = null
 
+    if (con) tasks = Windows()
+    else tasks = Linux()
+
+    tasks.startBluetooth()
+    tasks.startWifi()
+}
+
+
+class Windows : Tasks {
+    override fun startWifi() {
+        println("start wifi Windows")
+    }
+
+    override fun startBluetooth() {
+        println("start bluetooth Windows")
+    }
+}
+
+class Linux : Tasks {
+    override fun startWifi() {
+        println("start wifi Linux")
+    }
+
+    override fun startBluetooth() {
+        println("start bluetooth Linux")
+    }
+}
+
+
+interface Tasks {
+
+    fun startWifi()
+
+    fun startBluetooth()
 }
